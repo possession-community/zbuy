99f6913f1b63f4dd2ce9aa1b923875b665d26318
2vg <mail@nyaa.gg>
2025-06-10 00:42:03 +0900

    feat: rework

---
+++ src/knockback/knockback.cs
@@ -0,0 +1,72 @@
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Utils;
+using static Zbuy.Config;
+
+namespace Zbuy;
+
+public static class KnockbackSystem
+{
+    public static void ApplyKnockback(CCSPlayerController victim, CCSPlayerController attacker, string weaponName, float damage)
+    {
+        if (!Zbuy.Instance.Config.EnableKnockback)
+            return;
+            
+        if (victim == null || attacker == null || victim == attacker)
+            return;
+            
+        if (victim.PlayerPawn.Value == null || attacker.PlayerPawn.Value == null)
+            return;
+
+        if (victim.PlayerPawn.Value.LifeState != (byte)LifeState_t.LIFE_ALIVE)
+            return;
+            
+        if (!Zbuy.Instance.Config.WeaponDatas.TryGetValue(weaponName, out WeaponData? weaponData))
+            return;
+            
+        if (!weaponData.KnockbackScale.HasValue || weaponData.KnockbackScale.Value <= 0)
+            return;
+            
+        Vector? victimPos = victim.PlayerPawn.Value.AbsOrigin;
+        Vector? attackerPos = attacker.PlayerPawn.Value.AbsOrigin;
+        
+        if (victimPos == null || attackerPos == null)
+            return;
+            
+        Vector direction = victimPos - attackerPos;
+        Vector normalizedDirection = NormalizeVector(direction);
+        
+        float knockbackForce = damage * weaponData.KnockbackScale.Value;
+        
+        Vector knockbackVelocity = new Vector(
+            normalizedDirection.X * knockbackForce,
+            normalizedDirection.Y * knockbackForce,
+            Math.Max(0, normalizedDirection.Z * knockbackForce * 0.5f)
+        );
+        
+        if (victim.PlayerPawn.Value.AbsVelocity != null)
+        {
+            victim.PlayerPawn.Value.AbsVelocity.X += knockbackVelocity.X;
+            victim.PlayerPawn.Value.AbsVelocity.Y += knockbackVelocity.Y;
+            victim.PlayerPawn.Value.AbsVelocity.Z += knockbackVelocity.Z;
+        }
+    }
+    
+    private static Vector NormalizeVector(Vector vector)
+    {
+        float x = vector.X;
+        float y = vector.Y;
+        float z = vector.Z;
+        
+        float magnitude = MathF.Sqrt(x * x + y * y + z * z);
+        
+        if (magnitude > 0.0f)
+        {
+            x /= magnitude;
+            y /= magnitude;
+            z /= magnitude;
+        }
+        
+        return new Vector(x, y, z);
+    }
+}
---
+++ src/zbuy.cs
@@ -0,0 +1,213 @@
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Core.Translations;
+using CounterStrikeSharp.API.Modules.Memory;
+using CounterStrikeSharp.API.Modules.Memory.DynamicFunctions;
+using CounterStrikeSharp.API.Modules.Utils;
+using static Zbuy.Config;
+using static Zbuy.Weapon;
+using static CounterStrikeSharp.API.Core.Listeners;
+
+namespace Zbuy;
+
+public class Zbuy : BasePlugin, IPluginConfig<Config>
+{
+    public override string ModuleName => "Zbuy";
+    public override string ModuleVersion => "2.0";
+    public override string ModuleAuthor => "uru, schwarper";
+
+    public Config Config { get; set; } = new Config();
+    public static Zbuy Instance { get; private set; } = new();
+
+    public override void Load(bool hotReload)
+    {
+        Instance = this;
+
+        VirtualFunctions.CBaseEntity_TakeDamageOldFunc.Hook(OnTakeDamage, HookMode.Pre);
+        VirtualFunctions.CCSPlayer_ItemServices_CanAcquireFunc.Hook(OnWeaponCanAcquire, HookMode.Pre);
+        
+        ConVarManager.Initialize();
+        BuySystem.RegisterCommands();
+    }
+
+    public override void Unload(bool hotReload)
+    {
+        VirtualFunctions.CBaseEntity_TakeDamageOldFunc.Unhook(OnTakeDamage, HookMode.Pre);
+        VirtualFunctions.CCSPlayer_ItemServices_CanAcquireFunc.Unhook(OnWeaponCanAcquire, HookMode.Pre);
+        
+        ConVarManager.Cleanup();
+    }
+
+    public void OnConfigParsed(Config config)
+    {
+        Config = config;
+    }
+
+    [GameEventHandler]
+    public HookResult OnWeaponFire(EventWeaponFire @event, GameEventInfo info)
+    {
+        if (@event.Userid is not { } player || player.PlayerPawn.Value?.WeaponServices?.ActiveWeapon.Value is not { } activeWeapon)
+            return HookResult.Continue;
+
+        if (!Config.WeaponDatas.TryGetValue(GetDesignerName(activeWeapon), out WeaponData? weaponData))
+            return HookResult.Continue;
+
+        if (weaponData.UnlimitedClip == true)
+            activeWeapon.Clip1 += 1;
+
+        if (weaponData.UnlimitedAmmo == true)
+            activeWeapon.ReserveAmmo[0] += 1;
+
+        if (weaponData.ReloadAfterShoot == true)
+        {
+            if (activeWeapon.As<CCSWeaponBase>().VData is not { } weaponVData)
+                return HookResult.Continue;
+
+            player!.ExecuteClientCommand("slot3");
+
+            Instance.AddTimer(0.1f, () =>
+            {
+                player.ExecuteClientCommand($"slot{(uint)weaponVData.GearSlot + 1}");
+            });
+        }
+
+        return HookResult.Continue;
+    }
+
+    [GameEventHandler]
+    public HookResult OnRoundStart(EventRoundStart @event, GameEventInfo info)
+    {
+        BuySystem.ResetAllPurchaseCounts();
+        return HookResult.Continue;
+    }
+
+    [GameEventHandler]
+    public HookResult OnItemEquip(EventItemEquip @event, GameEventInfo info)
+    {
+        if (@event.Userid is not { } player || player.PlayerPawn.Value?.WeaponServices?.ActiveWeapon.Value is not { } activeWeapon)
+            return HookResult.Continue;
+
+        string globalname = activeWeapon.Globalname;
+
+        if (!string.IsNullOrEmpty(globalname))
+        {
+            string model = GetFromGlobalName(globalname, GlobalNameData.ViewModel);
+            SetViewModel(player, model);
+        }
+
+        return HookResult.Continue;
+    }
+
+    [ListenerHandler<OnEntitySpawned>]
+    public void OnEntitySpawned(CEntityInstance entity)
+    {
+        if (!entity.DesignerName.StartsWith("weapon_"))
+            return;
+
+        if (!Config.WeaponDatas.TryGetValue(GetDesignerName(entity.As<CBasePlayerWeapon>()), out WeaponData? weaponData))
+            return;
+
+        if (entity.As<CCSWeaponBase>().VData is not CCSWeaponBaseVData weaponVData)
+            return;
+
+        if (weaponData.Clip.HasValue)
+            weaponVData.MaxClip1 = weaponData.Clip.Value;
+
+        if (weaponData.Ammo.HasValue)
+            weaponVData.PrimaryReserveAmmoMax = weaponData.Ammo.Value;
+    }
+
+    [ListenerHandler<OnEntityCreated>]
+    public void OnEntityCreated(CEntityInstance entity)
+    {
+        Server.NextWorldUpdate(() =>
+        {
+            CBasePlayerWeapon weapon = new(entity.Handle);
+
+            if (!entity.IsValid || weapon.OriginalOwnerXuidLow <= 0)
+                return;
+
+            if (!Config.WeaponDatas.TryGetValue(GetDesignerName(weapon), out WeaponData? weaponData) || string.IsNullOrEmpty(weaponData.ViewModel))
+                return;
+
+            if (FindTargetFromWeapon(weapon) is not { } player)
+                return;
+
+            CBasePlayerWeapon? activeWeapon = player.PlayerPawn.Value?.WeaponServices?.ActiveWeapon.Value;
+            UpdateModel(player, weapon, weaponData.ViewModel, weaponData.WorldModel, weapon == activeWeapon);
+        });
+    }
+
+    [ListenerHandler<OnServerPrecacheResources>]
+    public void OnServerPrecacheResources(ResourceManifest manifest)
+    {
+        foreach (KeyValuePair<string, WeaponData> weaponData in Config.WeaponDatas)
+        {
+            if (!string.IsNullOrEmpty(weaponData.Value.ViewModel))
+                manifest.AddResource(weaponData.Value.ViewModel);
+
+            if (!string.IsNullOrEmpty(weaponData.Value.WorldModel))
+                manifest.AddResource(weaponData.Value.WorldModel);
+        }
+    }
+
+    public HookResult OnTakeDamage(DynamicHook hook)
+    {
+        if (hook.GetParam<CEntityInstance>(0).DesignerName is not "player")
+        {
+            return HookResult.Continue;
+        }
+
+        CTakeDamageInfo info = hook.GetParam<CTakeDamageInfo>(1);
+        CBaseEntity? weapon = info.Ability.Value;
+
+        if (weapon == null)
+            return HookResult.Continue;
+
+        string weaponName = GetDesignerName(weapon.As<CBasePlayerWeapon>());
+        
+        if (!Config.WeaponDatas.TryGetValue(weaponName, out WeaponData? weaponData))
+            return HookResult.Continue;
+
+        if (weaponData.OnlyHeadshot == true && info.GetHitGroup() != HitGroup_t.HITGROUP_HEAD)
+            return HookResult.Handled;
+
+        SetDamage(info, weaponData);
+        
+        if (Config.EnableKnockback && weaponData.KnockbackScale.HasValue && weaponData.KnockbackScale.Value > 0)
+        {
+            var victim = hook.GetParam<CEntityInstance>(0).As<CCSPlayerController>();
+            var attacker = info.Attacker.Value?.As<CCSPlayerController>();
+            
+            if (victim != null && attacker != null)
+            {
+                KnockbackSystem.ApplyKnockback(victim, attacker, weaponName, info.Damage);
+            }
+        }
+        
+        return HookResult.Continue;
+    }
+
+    public HookResult OnWeaponCanAcquire(DynamicHook hook)
+    {
+        CCSWeaponBaseVData vdata = VirtualFunctions.GetCSWeaponDataFromKeyFunc.Invoke(-1, hook.GetParam<CEconItemView>(1).ItemDefinitionIndex.ToString()) ?? throw new Exception("Failed to get CCSWeaponBaseVData");
+
+        if (!Config.WeaponDatas.TryGetValue(vdata.Name, out WeaponData? weaponData))
+            return HookResult.Continue;
+
+        if (hook.GetParam<CCSPlayer_ItemServices>(0).Pawn.Value?.Controller.Value?.As<CCSPlayerController>() is not CCSPlayerController player)
+            return HookResult.Continue;
+
+        if (!IsRestricted(player, vdata.Name, weaponData, hook.GetParam<AcquireMethod>(2)))
+        {
+            return HookResult.Continue;
+        }
+
+        if (!player.IsBot)
+            Instance.Localizer.ForPlayer(player, "You cannot use this weapon", vdata.Name);
+
+        hook.SetReturn(AcquireResult.NotAllowedByProhibition);
+        return HookResult.Handled;
+    }
+}
